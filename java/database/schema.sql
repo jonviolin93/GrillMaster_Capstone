2d7b31b1d21865119112dd8f488e2b34fcfc0607
Ellis Hay <27mlb27@gmail.com>
2023-04-10 14:24:05 -0400

    wrote SQL queries to create tables

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -10,4 +10,95 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
+DROP TABLE IF EXISTS food_category CASCADE;
+CREATE TABLE food_category(
+	category_id serial,
+	name varchar(50),
+	CONSTRAINT PK_category PRIMARY KEY (category_id)
+);
+
+DROP TABLE IF EXISTS food CASCADE;
+CREATE TABLE food (
+	food_id serial,
+	name varchar(50) NOT NULL,
+	image varchar(250),
+	category_id int NOT NULL,
+	CONSTRAINT PK_food PRIMARY KEY(food_id),
+	CONSTRAINT FK_category FOREIGN KEY(category_id) REFERENCES food_category(category_id)
+);
+
+DROP TABLE IF EXISTS menu CASCADE;
+CREATE TABLE menu(
+	menu_id serial,
+	is_favorited boolean DEFAULT false,
+	CONSTRAINT PK_menu PRIMARY KEY (menu_id)
+);
+
+DROP TABLE IF EXISTS cookout CASCADE;
+CREATE TABLE cookout(
+	cookout_id serial,
+	name VARCHAR(50) NOT NULL,
+	cookout_date date NOT NULL,
+	cookout_time time NOT NULL,
+	location VARCHAR(100) NOT NULL,
+	description TEXT,
+	menu_id int,
+	CONSTRAINT PK_cookout PRIMARY KEY (cookout_id),
+	CONSTRAINT FK_menu FOREIGN KEY(menu_id) REFERENCES menu(menu_id)
+);
+
+DROP TABLE IF EXISTS role CASCADE;
+CREATE TABLE role(
+	role_id serial,
+	name varchar(15),
+	CONSTRAINT PK_role PRIMARY KEY (role_id)
+);
+
+DROP TABLE IF EXISTS user_cookout CASCADE;
+CREATE TABLE user_cookout(
+	user_id int,
+	cookout_id int,
+	role_id int,
+	PRIMARY KEY(user_id, cookout_id, role_id),
+	CONSTRAINT FK_user FOREIGN KEY (user_id) REFERENCES users(user_id),
+	CONSTRAINT FK_cookout FOREIGN KEY (cookout_id) REFERENCES cookout(cookout_id),
+	CONSTRAINT FK_role FOREIGN KEY (role_id) REFERENCES role(role_id)
+);
+	
+
+DROP TABLE IF EXISTS food_menu CASCADE;
+CREATE TABLE food_menu(
+	food_id int NOT NULL,
+	menu_id int NOT NULL,
+	CONSTRAINT FK_food FOREIGN KEY(food_id) REFERENCES food(food_id),
+	CONSTRAINT FK_menu FOREIGN KEY(menu_id) REFERENCES menu(menu_id)
+);
+
+DROP TABLE IF EXISTS cookout_order CASCADE;
+CREATE TABLE cookout_order(
+	order_id serial,
+	user_id int not null,
+	is_complete boolean DEFAULT false,
+	CONSTRAINT PK_order PRIMARY KEY (order_id),
+	CONSTRAINT FK_user FOREIGN KEY(user_id) REFERENCES users(user_id)
+);
+
+DROP TABLE IF EXISTS order_food CASCADE;
+CREATE TABLE order_food(
+	order_id int not null,
+	food_id int not null,
+	CONSTRAINT FK_order FOREIGN KEY(order_id) REFERENCES cookout_order(order_id),
+	CONSTRAINT FK_food FOREIGN KEY(food_id) REFERENCES food(food_id)
+);
+
+INSERT INTO food_category(name) VALUES ('Main');
+INSERT INTO food_category(name) VALUES ('Side');
+INSERT INTO food_category(name) VALUES ('Alcohol');
+INSERT INTO food_category(name) VALUES ('Soft Drink');
+INSERT INTO food_category(name) VALUES ('Dessert');	
+
+INSERT INTO role(name) VALUES ('Host');	
+INSERT INTO role(name) VALUES ('Grill Master');	
+INSERT INTO role(name) VALUES ('Attendee');	
+
 COMMIT TRANSACTION;
