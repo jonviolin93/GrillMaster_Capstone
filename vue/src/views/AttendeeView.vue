3a7b8515870e226c847cab4b5ce5bcd17e89f146
Redding Haines <reddinghaines@gmail.com>
2023-04-14 15:35:33 -0400

    vfixed views

---
+++ vue/src/views/AttendeeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div><h1>What's happenin with the cookout</h1>
+     <menu-items/>
+      <attendees-list/>
+  </div>
+
+</template>
+<script>
+
+</script>
+<style scoped>
+
+</style>
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="home">
+   
+    <div id="nav">
+  
+      <router-link v-bind:to="{ name: 'host' }">Host</router-link>&nbsp;|&nbsp;
+   
+    </div>
+
+
+    
+    <router-view />
+    <section id="host">
+      <router-link to="/create-cookout">
+      <button id="create-cookout"></button>
+      </router-link>
+      <router-link v-bind:to="{ name: 'host' }">Home</router-link>&nbsp;|&nbsp;
+    </section>
+    <section id="attendee"></section>
+    <section id="chef"></section>
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
     <cookout-list/>
@@ -16,3 +34,6 @@ export default {
   }
 };
 </script>
+<style scoped>
+
+</style>
