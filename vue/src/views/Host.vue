0fe23220be9d850bf63f3d2163fb69c52c3c5cd1
Jonathan Moser <jonviolin93@gmail.com>
2023-04-16 19:12:33 -0400

    Some work on connecting

--- vue/src/views/Host.vue
+++ vue/src/views/Host.vue
@@ -1,9 +1,12 @@
 <template>
-  <div>
+  <div class="main">
       <h1>Information regarding this most honorable cookout</h1>
+  
+  
   <cookout-card/>
   <attendees-list />
   <menu-items />
+  <place-order/>
   </div>
 </template>
 
@@ -12,16 +15,25 @@
 import AttendeesList from "../components/AttendeesList.vue"
 import MenuItems from "../components/AttendeesList.vue"
 import CookoutCard from "../components/CookoutCard.vue"
+import PlaceOrder from "../components/PlaceOrder.vue"
+
 export default {
     components:{ 
-     
       AttendeesList,
       MenuItems,
-      CookoutCard }
+      CookoutCard,
+      PlaceOrder }
 
 }
 </script>
 
 <style>
+.main {
+  display: flex;
+  grid-template-columns: 1fr 1fr 1fr;
+
+}
+
+
 
 </style>
