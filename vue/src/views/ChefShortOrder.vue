290f7592480926ea3cc3a39b44045dbd47a40410
Redding Haines <reddinghaines@gmail.com>
2023-04-18 21:41:50 -0400

    styled grill master screen

--- vue/src/views/ChefShortOrder.vue
+++ vue/src/views/ChefShortOrder.vue
@@ -2,10 +2,11 @@
   <div>
     <header>Grill Master</header>
     <h1>ORDERS</h1>
-    <div v-for="order in this.$store.state.orders" v-bind:key="order.id">
+    <div id="order-boxes" class="flexbox" v-for="order in this.$store.state.orders" v-bind:key="order.id">
       {{order.username}} Time:{{order.time}}
-      <div v-for="food in order.foodList" v-bind:key="food.id">{{food.name}} Quantity:{{food.quantity}}</div>
-      
+      <div id="order-food">
+      <div id="order-food" v-for="food in order.foodList" v-bind:key="food.id">{{food.name}} Quantity:{{food.quantity}}</div>
+      </div>
       <button v-if="!order.complete" v-on:click="bumpOrder(order.id)">BUMP</button>
       <p v-else>Order Up!</p>
     
@@ -61,16 +62,37 @@ justify-content: center;
 }
 h1{
   display: flex;
- 
+ align-items: center;
+ justify-content: center;
   flex-direction: row;
   background-color: #BB2B1B;
   border-radius: 10px; 
 max-width:65%;
+padding-left: auto;
 }
 button{
   display: inline-block;
-  background-color: rgb(231, 163, 15);
+  background-color:#BB2B1B;
   border-radius: 10px;
 
 }
+div#order-boxes{
+  background-color:  rgb(231, 163, 15);
+  display: inline-flex;
+  border: black;
+  border-radius: 15px 15px 15px 15px;
+  max-width: 100%;
+margin-top: 8px;
+  justify-content: space-between;
+
+
+}
+div#order-food{
+  display: flex;
+  flex-wrap: wrap;
+border: 10px, black;
+border-bottom: 10px;
+
+}
+
 </style>
